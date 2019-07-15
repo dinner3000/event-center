4de3438f8da55539349b20eecf7254edee9c62f0
Gary Zhu <dinner3000@gmail.com>
2019-07-15 12:07:26 +0800

    refact: improve messaging classes

--- event-forwarder/src/main/java/com/chinaccs/exhibit/ucsp/eventcenter/eventforwarder/service/impl/ForwardTaskExecuteServiceImpl.java
+++ event-forwarder/src/main/java/com/chinaccs/exhibit/ucsp/eventcenter/eventforwarder/service/impl/ForwardTaskExecuteServiceImpl.java
@@ -2,11 +2,13 @@ package com.chinaccs.exhibit.ucsp.eventcenter.eventforwarder.service.impl;
 
 import com.chinaccs.exhibit.ucsp.eventcenter.eventdata.entity.EventForwardLogEntity;
 import com.chinaccs.exhibit.ucsp.eventcenter.eventdata.service.EventForwardLogService;
+import com.chinaccs.exhibit.ucsp.eventcenter.eventforwarder.agent.MessagingAgent;
 import com.chinaccs.exhibit.ucsp.eventcenter.eventforwarder.constant.EventForwardStatus;
 import com.chinaccs.exhibit.ucsp.eventcenter.eventforwarder.service.ForwardTaskExecuteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -19,6 +21,10 @@ public class ForwardTaskExecuteServiceImpl implements ForwardTaskExecuteService 
     @Autowired
     private EventForwardLogService eventForwardLogService;
 
+    @Autowired
+    @Qualifier("com.chinaccs.exhibit.ucsp.eventcenter.eventforwarder.agent.impl.SMSMessagingAgent")
+    private MessagingAgent smsMessagingAgent;
+
     @Override
     public void forward(EventForwardLogEntity eventForwardLogEntity) {
 
@@ -26,7 +32,7 @@ public class ForwardTaskExecuteServiceImpl implements ForwardTaskExecuteService 
         try {
             logger.debug("forward to external api => url: {}, targets: {}",
                 eventForwardLogEntity.getTplText(), eventForwardLogEntity.getTargets());
-            logger.debug("add real logic here");
+            smsMessagingAgent.invoke(eventForwardLogEntity);
 
             if (eventForwardLogEntity.getId() % 2 != 0) {
                 throw new RuntimeException("Simulate forward failure");
